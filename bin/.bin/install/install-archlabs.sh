00cc7468eafa07e534002e8d6eb4f47d8d4267ff
Daniel Hammer <braineniac@protonmail.com>
2018-09-30 22:03:35 +0200

    modified zsh, initial stow setup

--- bin/.bin/install/install-archlabs.sh
+++ bin/.bin/install/install-archlabs.sh
@@ -3,8 +3,24 @@
 sudo pacman -Suy --noconfirm
 sudo pacman -S stow syncthing-gtk rxvt-unicode vim --noconfirm
 
-#symlinks
-git clone 
+###symlinks###
+cd /home/$USER/.dotfiles
+
+#backups
+mv /home/$USER/.config/conky /home/$USER/.config/.conky-old
+mv /home/$USER/.config/i3 /home/$USER/.config/.i3-old
+mv /home/$USER/.config/polybar /home/$USER/.config/.polybar-old
+mv /home/$USER/.config/jgmenu /home/$USER/.config/.jgmenu-old
+mv /home/$USER/.config/ranger /home/$USER/.config/.ranger-old
+mv /home/$USER/.config/termite /home/$USER/.config/.termite-old
+mv /home/$USER/.config/urxvt /home/$USER/.config/.urxvt-old
+mv /home/$USER/.config/tint2 /home/$USER/.config/.tint2-old
+mv /home/$USER/.vimrc /home/$USER/.vimrc-old
+mv /home/$USER/.zshrc /home/$USER/.zshrc-old
+mv /home/$USER/.zprofile /home/$USER/.zprofile-old
+
+#stow
+stow /home/$USER/.dotfiles/*
 
 #vim setup and install
 aurman -S vim-youcompleteme-git nerd-fonts-complete --noconfirm --pgp-fetch --skip-news
